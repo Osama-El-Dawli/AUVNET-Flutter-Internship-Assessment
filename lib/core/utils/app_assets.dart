225d8e943b701666b58de71fd76f3d2e4831a031
Osama Saeid <osamasaeed283@gmail.com>
2025-06-16 05:19:05 +0300

    add flutter native splash screen

--- lib/core/utils/app_assets.dart
+++ lib/core/utils/app_assets.dart
@@ -1,13 +1,17 @@
 // ignore_for_file: prefer_single_quotes
 class Assets {
   Assets._();
-
+  
   /// Assets for imagesLogo
   /// assets/images/logo.png
   static const String imagesLogo = "assets/images/logo.png";
 
+  /// Assets for imagesNativeSplash
+  /// assets/images/native_splash.png
+  static const String imagesNativeSplash = "assets/images/native_splash.png";
+
   /// Assets for imagesOnBoardingCircle
   /// assets/images/on_boarding_circle.svg
-  static const String imagesOnBoardingCircle =
-      "assets/images/on_boarding_circle.svg";
+  static const String imagesOnBoardingCircle = "assets/images/on_boarding_circle.svg";
 }
+
