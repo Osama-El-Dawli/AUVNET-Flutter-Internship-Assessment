b96f426634da13cfa45b7d684dc2f338fdf971fe
Osama Saeid <osamasaeed283@gmail.com>
2025-06-16 05:05:49 +0300

    finish splash logic

--- lib/features/splash/presentation/widgets/splash_view_body.dart
+++ lib/features/splash/presentation/widgets/splash_view_body.dart
@@ -1,8 +1,24 @@
 import 'package:auvnet/core/utils/app_assets.dart';
+import 'package:auvnet/features/on_boarding/presentation/views/on_boarding_view.dart';
 import 'package:flutter/material.dart';
 
-class SplashViewBody extends StatelessWidget {
+class SplashViewBody extends StatefulWidget {
   const SplashViewBody({super.key});
+
+  @override
+  State<SplashViewBody> createState() => _SplashViewBodyState();
+}
+
+class _SplashViewBodyState extends State<SplashViewBody> {
+  @override
+  void initState() {
+    Future.delayed(const Duration(seconds: 2), () {
+      if (mounted) {
+        Navigator.pushReplacementNamed(context, OnBoardingView.routeName);
+      }
+    });
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
