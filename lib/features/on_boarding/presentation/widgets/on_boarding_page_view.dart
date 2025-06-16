a46c647821b22490611e383068478b4857f7e6f6
Osama Saeid <osamasaeed283@gmail.com>
2025-06-16 16:13:24 +0300

    handle navigation between on boarding page view and navigation to login view

--- lib/features/on_boarding/presentation/widgets/on_boarding_page_view.dart
+++ lib/features/on_boarding/presentation/widgets/on_boarding_page_view.dart
@@ -1,12 +1,19 @@
 import 'package:auvnet/features/on_boarding/presentation/widgets/on_boarding_page_view_item.dart';
 import 'package:flutter/material.dart';
 
-class OnBoardingPageView extends StatelessWidget {
-  const OnBoardingPageView({super.key});
+class OnBoardingPageView extends StatefulWidget {
+  const OnBoardingPageView({super.key, required this.pageController});
+  final PageController pageController;
 
+  @override
+  State<OnBoardingPageView> createState() => _OnBoardingPageViewState();
+}
+
+class _OnBoardingPageViewState extends State<OnBoardingPageView> {
   @override
   Widget build(BuildContext context) {
     return PageView(
+      controller: widget.pageController,
       children: [
         OnBoardingPageViewItem(
           title: 'all-in-one delivery',
--- lib/features/on_boarding/presentation/widgets/on_boarding_view_body.dart
+++ lib/features/on_boarding/presentation/widgets/on_boarding_view_body.dart
@@ -1,17 +1,54 @@
 import 'package:auvnet/core/utils/app_assets.dart';
 import 'package:auvnet/core/utils/app_text_styles.dart';
 import 'package:auvnet/core/widgets/custom_button.dart';
+import 'package:auvnet/features/auth/presentation/views/login_view.dart';
 import 'package:auvnet/features/on_boarding/presentation/widgets/on_boarding_page_view.dart';
 import 'package:flutter/material.dart';
 
-class OnBoardingViewBody extends StatelessWidget {
+class OnBoardingViewBody extends StatefulWidget {
   const OnBoardingViewBody({super.key});
+
+  @override
+  State<OnBoardingViewBody> createState() => _OnBoardingViewBodyState();
+}
+
+class _OnBoardingViewBodyState extends State<OnBoardingViewBody> {
+  final PageController _pageController = PageController();
+  int _currentPage = 0;
+
+  void nextPage() {
+    _currentPage++;
+    _pageController.animateToPage(
+      _currentPage,
+      duration: const Duration(milliseconds: 500),
+      curve: Curves.easeInOut,
+    );
+  }
+
+  @override
+  void initState() {
+    _pageController.addListener(() {
+      setState(() {
+        final newPage = _pageController.page?.round() ?? 0;
+        if (newPage != _currentPage) {
+          _currentPage = newPage;
+          setState(() {});
+        }
+      });
+    });
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Column(
       children: [
-        Expanded(child: OnBoardingPageView()),
+        Expanded(child: OnBoardingPageView(pageController: _pageController)),
         const SizedBox(height: 52),
         Stack(
           clipBehavior: Clip.none,
@@ -25,14 +62,31 @@ class OnBoardingViewBody extends StatelessWidget {
               padding: const EdgeInsets.symmetric(horizontal: 40),
               child: Column(
                 children: [
-                  CustomButton(text: 'Get Started', onPressed: () {}),
+                  CustomButton(
+                    text: 'Get Started',
+                    onPressed: () {
+                      Navigator.pushReplacementNamed(
+                        context,
+                        LoginView.routeName,
+                      );
+                    },
+                  ),
                   const SizedBox(height: 14),
                   TextButton(
                     style: TextButton.styleFrom(
                       visualDensity: VisualDensity.compact,
                       tapTargetSize: MaterialTapTargetSize.shrinkWrap,
                     ),
-                    onPressed: () {},
+                    onPressed: () {
+                      if (_currentPage < 2) {
+                        nextPage();
+                      } else {
+                        Navigator.pushReplacementNamed(
+                          context,
+                          LoginView.routeName,
+                        );
+                      }
+                    },
                     child: Text(
                       'Next',
                       style: AppTextStyles.regular14.copyWith(
